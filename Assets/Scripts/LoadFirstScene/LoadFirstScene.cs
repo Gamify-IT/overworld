dcffa39a91cf360e36ae34e04a323e538e31eeab
st150166 <m.linder1@gmx.de>
2022-07-03 19:25:04 +0200

    Update LoadFirstScene.cs

--- Assets/Scripts/LoadFirstScene/LoadFirstScene.cs
+++ Assets/Scripts/LoadFirstScene/LoadFirstScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneLoader : MonoBehaviour
+public class LoadFirstScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
