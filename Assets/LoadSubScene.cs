9b0cacf4d72f88e22f15947c2691f44014c89711
Martin Lautenschlager <martinlautenschlager@protonmail.com>
2022-07-07 13:31:04 +0200

    add fade to scene transition

--- Assets/LoadSubScene.cs
+++ Assets/LoadSubScene.cs
@@ -6,13 +6,40 @@ using UnityEngine.SceneManagement;
 public class LoadSubScene : MonoBehaviour
 {
     public string sceneToLoad;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float loadingTime;
+
+    private void Awake()
+    {
+        if(fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D player)
     {
         if (player.tag == "Player")
         {
-            SceneManager.LoadScene(sceneToLoad);
-
-            SceneManager.LoadScene("Player HUD", LoadSceneMode.Additive);
+            StartCoroutine(FadeCoroutine());
         }
+    }
+
+    public IEnumerator FadeCoroutine()
+    {
+        if(fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(loadingTime);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+        AsyncOperation loadHUD = SceneManager.LoadSceneAsync("Player HUD", LoadSceneMode.Additive);
+        while (!asyncOperation.isDone && !loadHUD.isDone)
+        {
+            yield return null;
+        }
+        
     }
 }
