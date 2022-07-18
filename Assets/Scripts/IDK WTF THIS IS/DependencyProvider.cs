780f1d0de39aa829a7d0ccc97ce73d2c3d8c404a
Ilijaz Mehmedovic <mehmedovic.ilijaz@gmail.com>
2022-07-18 21:27:50 +0200

    removed old unused scripts

--- Assets/Scripts/IDK WTF THIS IS/DependencyProvider.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class DependencyProvider : MonoBehaviour
-{
-    public static DependencyProvider instance { get { { return FindObjectOfType<DependencyProvider>(); } } }
-
-    private EventSystemExampleDependencies _eventSystemExampleDependencies = new EventSystemExampleDependencies();
-    public EventSystemExampleDependencies eventSystemExampleDependencies { get { return _eventSystemExampleDependencies; } }
-}
