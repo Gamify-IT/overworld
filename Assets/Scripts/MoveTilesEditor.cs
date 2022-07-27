ed276c1ea35ae2f0bf3dd8e89882315bb4ce0ef4
Ilijaz Mehmedovic <mehmedovic.ilijaz@gmail.com>
2022-07-27 22:29:01 +0200

    add script that moves tiles after they get distorted when updating the rendering order

---
+++ Assets/Scripts/MoveTilesEditor.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
+
+public class MoveTilesEditor : EditorWindow
+{
+    [MenuItem("Window/Custom/MoveTiles")]
+    public static void ShowWindow()
+    {
+        GetWindow<MoveTilesEditor>("MoveTiles");
+    }
+
+    public Tilemap tilemap;
+    public TileBase tileBase;
+    public Vector3 translation;
+
+    void OnGUI()
+    {
+        tilemap = (Tilemap) (EditorGUILayout.ObjectField(tilemap, typeof(Tilemap), true));
+        tileBase = (TileBase) (EditorGUILayout.ObjectField(tileBase, typeof(TileBase), true));
+        translation = EditorGUILayout.Vector3Field("translation: ", translation);
+
+        if (GUILayout.Button("Execute")) {
+            Vector3Int size = tilemap.size;
+            Vector3Int origin = tilemap.origin;
+
+            for (int x = origin.x; x < origin.x + size.x; x++) {
+                for (int y = origin.y; y < origin.y + size.y; y++) {
+                    TileBase tileBase = tilemap.GetTile(new Vector3Int(x, y));
+                    if (tileBase == this.tileBase) {
+                        Debug.Log("found tile at " + x + ", " + y);
+                        tilemap.SetTransformMatrix(new Vector3Int(x, y), Matrix4x4.TRS(translation, Quaternion.Euler(0f, 0f, 0f), Vector3.one));
+                    }
+                }
+            }
+        }
+    }
+}
