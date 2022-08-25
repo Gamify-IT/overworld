c9ee1078010912995aae2d996859206bbcc761e6
WuestFlorian <st171957@stud.uni-stuttgart.de>
2022-08-25 12:00:18 +0200

    Rework world loading and data setting

--- Assets/Scripts/Scene Loading/loadmaps.cs
+++ Assets/Scripts/Scene Loading/loadmaps.cs
@@ -1,55 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
 
 public class loadmaps : MonoBehaviour
 {
-    public string sceneLoadOne;
-    public string sceneLoadTwo;
-    public bool loadingFinished;
-    public int sceneToLoadIndex;
+    public string sceneOrigin;
+    public string sceneDestination;
+    public int sceneDestinationIndex;
 
     public void OnTriggerEnter2D(Collider2D playerCollision)
     {
-        if (playerCollision.tag == "Player" && loadingFinished)
+        if (playerCollision.tag == "Player")
         {
-            StartCoroutine(LoadUnloadWorlds(sceneLoadOne, sceneLoadTwo));
+            setupWorld();
         }
     }
 
-    public IEnumerator LoadUnloadWorlds(string loadWorldOne, string loadWorldTwo)
+    //setup world entered
+    //  - if already active, do nothing
+    //  - else: load scene and set data
+    private async UniTask setupWorld()
     {
-        loadingFinished = false;
-        AsyncOperation asyncOperationLoadOne = null;
-        AsyncOperation asyncOperationLoadTwo = null;
-
-        if (!SceneManager.GetSceneByName(loadWorldOne).isLoaded)
+        if (SceneManager.GetSceneByName(sceneDestination).isLoaded)
         {
-            asyncOperationLoadOne = SceneManager.LoadSceneAsync(loadWorldOne, LoadSceneMode.Additive);
+            return;
         }
-        if (!SceneManager.GetSceneByName(loadWorldTwo).isLoaded)
-        {
-            asyncOperationLoadTwo = SceneManager.LoadSceneAsync(loadWorldTwo, LoadSceneMode.Additive);
-        }
+        await loadWorld();
+        GameManagerV2.instance.setData(sceneDestinationIndex, 0);
+    }
 
-        for(int sceneIndex = 0;sceneIndex < SceneManager.sceneCount; sceneIndex++)
+    //load needed world and unload not needed worlds, if present
+    private async UniTask loadWorld()
+    {
+        await SceneManager.LoadSceneAsync(sceneDestination, LoadSceneMode.Additive);
+
+        for (int sceneIndex = 0; sceneIndex < SceneManager.sceneCount; sceneIndex++)
         {
             string tempSceneName = SceneManager.GetSceneAt(sceneIndex).name;
-            if (!tempSceneName.Equals("Player")  && !tempSceneName.Equals("Player HUD") && !tempSceneName.Equals(loadWorldOne) && !tempSceneName.Equals(loadWorldTwo))
+            if (!tempSceneName.Equals("Player") && !tempSceneName.Equals("Player HUD") && !tempSceneName.Equals(sceneOrigin) && !tempSceneName.Equals(sceneDestination))
             {
-                SceneManager.UnloadSceneAsync(tempSceneName);
+                await SceneManager.UnloadSceneAsync(tempSceneName);
             }
         }
-
-        while (
-            (asyncOperationLoadOne != null && !asyncOperationLoadOne.isDone) && 
-            (asyncOperationLoadTwo != null && !asyncOperationLoadTwo.isDone))
-        {
-            yield return null;
-        }
-        loadingFinished = true;
-        GameManagerV2.instance.setData(sceneToLoadIndex, 0);
     }
 }
