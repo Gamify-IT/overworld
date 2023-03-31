c758ef6eb9a8a4164a05c85f2bb95b75f895b1ea
WuestFlorian <102306228+WuestFlorian@users.noreply.github.com>
2023-03-31 13:49:03 +0200

    Add achievement saving on game close

---
+++ Assets/Scripts/HUD/CloseGame.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class CloseGame : MonoBehaviour
+{
+    /// <summary>
+    ///     Import of the overworld close methode
+    /// </summary>
+    [DllImport("__Internal")]
+    private static extern void CloseOverworld();
+
+    /// <summary>
+    ///     This function saves all achievement progress and then closes the game
+    /// </summary>
+    public async void CloseButtonPressed()
+    {
+        bool success = await GameManager.Instance.SaveAchievements();
+        if(success)
+        {
+            Debug.Log("Saved all achievement progress");
+        }
+        else
+        {
+            Debug.Log("Could not save all achievement progress");
+        }
+        CloseOverworld();
+    }
+}
