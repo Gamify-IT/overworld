7c5185287322a7e7dae62dfeb54380aee29f0d37
WuestFlorian <102306228+WuestFlorian@users.noreply.github.com>
2023-11-23 17:55:12 +0100

    Add info to inspector UI
    Reworked the information system and added it to the inspector UI

--- Assets/Scripts/WorldGeneration/UI/GeneratorUI.cs
+++ Assets/Scripts/WorldGeneration/UI/GeneratorUI.cs
@@ -14,12 +14,13 @@ public class GeneratorUI : MonoBehaviour
     private AreaInformation areaIdentifier;
     private AreaInformationData areaInformation;
 
+    [SerializeField] InterfaceInfo infoUI;
+
     //Panels
     [SerializeField] private GameObject generatorPanel;
     [SerializeField] private GameObject smallGeneratorPanel;
     [SerializeField] private GameObject areaSettings;
     [SerializeField] private GameObject content;
-    [SerializeField] private GameObject feedbackPanel;
 
     //Area Settings
     [SerializeField] private TMP_InputField sizeX;    
@@ -66,11 +67,6 @@ public class GeneratorUI : MonoBehaviour
     [SerializeField] private Button generateAllContentButton;
     [SerializeField] private Button saveAreaButton;
 
-    //Feedback
-    [SerializeField] private TextMeshProUGUI feedbackHeader;
-    [SerializeField] private ScrollRect feedbackScrollRect;
-    [SerializeField] private TextMeshProUGUI feedbackContent;
-    [SerializeField] private Button feedbackCloseButton;
     #endregion
 
     #region Setup
@@ -163,7 +159,6 @@ public class GeneratorUI : MonoBehaviour
         areaSettings.SetActive(true);
         content.SetActive(false);
         generatorPanel.SetActive(true);
-        feedbackPanel.SetActive(false);
     }
 
     /// <summary>
@@ -228,235 +223,6 @@ public class GeneratorUI : MonoBehaviour
 
     #endregion
 
-    #region Info Buttons
-
-    public void HelpButtonPressed()
-    {
-        if(feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "AREA GENERATION - INFORMATION";
-            string body = "INSPECT AREA: \n " +
-                "To inspect the current area use the 'GENERATOR MAP' Button in the to right corner. To get back, simply click the same button again. \n " +
-                "\n" +
-                "CAMERA MOVEMENT: \n " +
-                "To move the camera, hold down the left mouse botton and drag the map around with the mouse. To zoom in and out, use the mouse wheel. \n" +
-                "\n" +
-                "GENERATION PROCESS: \n" +
-                "\n" +
-                "LAYOUT GENERATION: \n " +
-                "To generate an area, you first need to generate a layout by selecting a 'Style', a 'Type' and an 'Accessable' value represeting the size of the walkable space. For more information, use the 'i' button next to them. \n" +
-                "Furthermore, you can either use a random 'Seed' or use a specific one. The seed, together with the other values, defines an area. Using the same 'Type' and 'Accessability' with the same 'Seed' results in the same layout. \n " +
-                "After selecting your parameters, use the 'GENERATE LAYOUT' button in the bottom middle. \n " +
-                "Once a good layout is generated, proceed with the 'CONTINUE' button in the bottom right. \n" +
-                "\n" +
-                "CONTENT GENERATION: \n " +
-                "Next, you can select the amount of object-type you want and add them with the respective 'GENERATE' button, or all of them at once with the 'GENERATE ALL' button, located in the bottom middle. \n" +
-                "You will receive feedback, whether or not the objects could be placed or not. In case they could not, you can either try again, change the values or go back and generate another layout using the 'RETURN' button. \n " +
-                "If you placed at least one minigame, you can save the area with the 'SAVE AREA' button. ";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void SizeInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "SIZE SETTING";
-            string body = "With the size setting, you can specifiy the dimensions of the generated area, with the 'X' input defining the width, the 'Y' input the height. \n " +
-                "You can only change the size of a dungeon area, a world area must have a fixed size.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void StyleInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "STYLE SETTING";
-            string body = "The style setting defines the theme of the generated area. Currently, there are four themes: \n" +
-                "\n" +
-                "'CAVE': An underground tunnel system \n" +
-                "\n" +
-                "'SAVANNA': A grassland enviroment surrounded by water and steep cliffs \n " +
-                "\n" +
-                "'BEACH': A collection of sandy islands surrounded by the sea, connected with pathways in shallow water \n" +
-                "\n" +
-                "'FOREST': A forest with pathways and clearings";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void TypeInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "TYPE SETTING";
-            string body = "The type setting defines the generator used and thereby which characteristics the area has. The different types are: \n" +
-                "\n" +
-                "'CELLULAR AUTOMATA': This generator results in smaller pathways, spread around the entirety of the area \n" +
-                "\n" +
-                "'DRUNKARDS WALK': This generator results in few bigger areas connected by pathways \n" +
-                "\n" +
-                "'ISLANDS - CA': This generator results in many smaller, well shaped islands, connected with narrow pathways \n" +
-                "\n" +
-                "'ISLANDS _ DW': This generator results in many smaller, chaoticly shaped islands, connected with narrow pathways";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void AccessableInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "ACCESSABLE SETTING";
-            string body = "The accessable setting defines, how much area the player will be able to explore. A higher value translates to more exploreable space.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void SeedInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "SEED SETTING";
-            string body = "The seed is used to generate different layouts. With the same seed and other settings, the exact same layout is reproduced. \n" +
-                "\n" +
-                "You can change the 'style' setting, to see the layout in different themes with only very slight adjustments. \n" +
-                "\n" +
-                "In case you want to use the same layout in another course, you can copy the seed and use it there with the same settings to effectively copy the layout.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void MinigamesInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "MINIGAME SPOTS";
-            string body = "A minigame spot represents a position for a minigame. There, students can enter a game focused on a part of the content of the lecture. \n" +
-                "\n" +
-                "The amount of minigame spots can be set with the slider and the currently selected amount can be placed with the 'GENERATE' button. \n " +
-                "To remove all minigame spots, simply set the slider to '0' and press the 'GENERATE' button. \n" +
-                "\n" +
-                "To enable or disable the icons on the layout, use the respective checkbox.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void NpcsInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "NPC SPOTS";
-            string body = "A npc spot represents a position for a npc. That is a Non-Player-Character, that is standing around and the students can talk to. \n" +
-                "\n" +
-                "The npcs are meant to provide additional information about the content required in a minigame, therefore, they are placed near their corresponding minigame. \n " +
-                "To ensure that, it is recommended to generate them after the minigame spots have been placed. \n " +
-                "\n" +
-                "The amount of npc spots can be set with the slider and the currently selected amount can be placed with the 'GENERATE' button. \n " +
-                "To remove all npc spots, simply set the slider to '0' and press the 'GENERATE' button. \n" +
-                "\n" +
-                "To enable or disable the icons on the layout, use the respective checkbox.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void BooksInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "BOOK SPOTS";
-            string body = "A book spot represents a position for a book. They are similar to npcs, but contain more content and continuous text. \n" +
-                "They are also, like npcs, placed near their corresponding minigame. To ensure that, it is recommended to generate them after the minigame spots have been placed \n " +
-                "\n" +
-                "The amount of book spots can be set with the slider and the currently selected amount can be placed with the 'GENERATE' button. \n " +
-                "To remove all book spots, simply set the slider to '0' and press the 'GENERATE' button. \n" +
-                "\n" +
-                "To enable or disable the icons on the layout, use the respective checkbox.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void TeleporterInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "TELEPORTER SPOTS";
-            string body = "A teleporter spot represents a position for a teleporter. The students can activate them and then use them instantly travel to another activated teleporter. \n " +
-                "That helps to get around the area faster and reduce walking times. \n " +
-                "\n " +
-                "The amount of teleporter spots can be set with the slider and the currently selected amount can be placed with the 'GENERATE' button. \n " +
-                "To remove all teleporter spots, simply set the slider to '0' and press the 'GENERATE' button. \n" +
-                "\n" +
-                "To enable or disable the icons on the layout, use the respective checkbox.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    public void DungeonsInfoButtonPressed()
-    {
-        if (feedbackPanel.activeSelf)
-        {
-            feedbackPanel.SetActive(false);
-        }
-        else
-        {
-            string header = "DUNGEON SPOTS";
-            string body = "A dungeon spot represents a position for a dungeon entrance. There, students can enter or leave a dungeon. \n" +
-                "\n" +
-                "The amount of dungeon spots can be set with the slider and the currently selected amount can be placed with the 'GENERATE' button. \n " +
-                "To remove all dungeon spots, simply set the slider to '0' and press the 'GENERATE' button. \n" +
-                "\n" +
-                "To enable or disable the icons on the layout, use the respective checkbox. \n " +
-                "\n" +
-                "A world area can have multiple dungeon spots, but is also allowed to have none at all. \n" +
-                "Each dungeon is required to have exactly one dungeon spot, which is the exit. Therefore, in a dungeon area, the amount of dungeon spots cannot be changed, but the position can still be changed using the 'GENERATE' button.";
-            DisplayFeedback(header, body, true);
-        }
-    }
-
-    #endregion
-
     public void MinimizeButtonPressed()
     {
         generatorPanel.SetActive(false);
@@ -485,7 +251,7 @@ public class GeneratorUI : MonoBehaviour
 
     private async void ResetArea()
     {
-        DisplayFeedback("RESETING AREA...", "RESETING THE AREA TO DEFAULT ...", false);
+        infoUI.DisplayInfo("RESETING AREA...", "RESETING THE AREA TO DEFAULT ...", false);
 
         stypeDropdown.value = 0;
         continueButton.interactable = false;
@@ -494,17 +260,17 @@ public class GeneratorUI : MonoBehaviour
 
         if(success)
         {
-            DisplayFeedback("AREA RESET", "RESET THE CURRENT AREA TO DEFAULT", true);
+            infoUI.DisplayInfo("AREA RESET", "RESET THE CURRENT AREA TO DEFAULT", true);
         }
         else
         {
-            DisplayFeedback("RESET", "COULD NOT RESET THE CURRENT AREA TO DEFAULT", true);
+            infoUI.DisplayInfo("RESET", "COULD NOT RESET THE CURRENT AREA TO DEFAULT", true);
         }
     }
 
     public void GenerateLayoutButtonPressed()
     {
-        DisplayFeedback("GENERATION LAYOUT...", "BLUB", false);
+        infoUI.DisplayInfo("GENERATION LAYOUT...", "BLUB", false);
 
         Vector2Int size = new Vector2Int(int.Parse(sizeX.text), int.Parse(sizeY.text));
         WorldStyle style = (WorldStyle) stypeDropdown.value;
@@ -518,7 +284,7 @@ public class GeneratorUI : MonoBehaviour
 
         ResetContentPanel();
 
-        DisplayFeedback("LAYOUT GENERATED", "SUCCESSFULLY GENERATED A LAYOUT", true);
+        infoUI.DisplayInfo("LAYOUT GENERATED", "SUCCESSFULLY GENERATED A LAYOUT", true);
     }
 
     /// <summary>
@@ -693,7 +459,7 @@ public class GeneratorUI : MonoBehaviour
             header = "GENERATION FAILED";
             content = "COULD NOT CREATE ALL OBJECTS, \n \n PLEASE TRY AGAIN, \n CHANGE THE AMOUNT OF OBJECTS, \n OR \n CREATE ANOTHER LAYOUT";
         }
-        DisplayFeedback(header, content, true);
+        infoUI.DisplayInfo(header, content, true);
     }
     #endregion
 
@@ -796,35 +562,18 @@ public class GeneratorUI : MonoBehaviour
 
     private async void SaveArea()
     {
-        DisplayFeedback("SAVING...", "SAVING THE CURRENT AREA...", false);
+        infoUI.DisplayInfo("SAVING...", "SAVING THE CURRENT AREA...", false);
 
         bool success = await uiManager.SaveArea();
 
         if(success)
         {
-            DisplayFeedback("AREA SAVED", "SAVED THE CURRENT AREA", true);
+            infoUI.DisplayInfo("AREA SAVED", "SAVED THE CURRENT AREA", true);
         }
         else
         {
-            DisplayFeedback("AREA NOT SAVED", "COULD NOT SAVE THE CURRENT AREA", true);
+            infoUI.DisplayInfo("AREA NOT SAVED", "COULD NOT SAVE THE CURRENT AREA", true);
         }        
-    }
-    #endregion
-
-    #region Feedback
-    public void FeedbackCloseButtonPressed()
-    {
-        feedbackPanel.SetActive(false);
-    }
-
-    private void DisplayFeedback(string header, string content, bool closeable)
-    {
-        feedbackHeader.text = header;
-        feedbackContent.text = content;
-        feedbackScrollRect.verticalNormalizedPosition = 1f;
-        feedbackCloseButton.gameObject.SetActive(closeable);
-
-        feedbackPanel.SetActive(true);
     }
     #endregion
 
