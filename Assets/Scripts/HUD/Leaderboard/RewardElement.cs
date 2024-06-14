6b638e31234054b872bcbc4fb4966e2c5e534a5f
SuyaneKurz <st185451@stud.uni-stuttgart.de>
2024-06-14 18:04:27 +0200

    create the reward elements
    create the ui element of the leaderboard

--- Assets/Scripts/HUD/Leaderboard/RewardElement.cs
+++ Assets/Scripts/HUD/Leaderboard/RewardElement.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RewardElement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Text playername;
+    [SerializeField] private TMP_Text reward;
+   
+    [SerializeField] private TMP_Text place; //platzierung anzeigen?
+    
 
-    // Update is called once per frame
-    void Update()
+    public void Setup(string playername,  int reward, int score, int place, int number)
     {
-        
+        this.playername.text = playername.Replace("_", " ");
+        this.reward.text = reward + "/" + score; //int kann ansonsten nicht als string gespeichert werden
+        this.place.text = place + "/" + number;
     }
 }
