460450e545bfea5601bca8e41c0abeba06c1548c
Leonard <weasleywalker@gmx.de>
2024-07-16 12:33:42 +0200

    added selector menu to properly start world generation

--- Assets/Scripts/WorldGeneration/AreaGeneratorManager.cs
+++ Assets/Scripts/WorldGeneration/AreaGeneratorManager.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 /// </summary>
 public class AreaGeneratorManager : MonoBehaviour
 {
+    public static AreaGeneratorManager Instance { get; private set; }
     /// <summary>
     ///     Import of the overworld close methode
     /// </summary>
@@ -19,14 +20,17 @@ public class AreaGeneratorManager : MonoBehaviour
     #region Attributes
     //UI
     [SerializeField] private CameraMovement cameraController;
-
     [SerializeField] private GameObject panel;
     [SerializeField] private TextMeshProUGUI infoText;
     [SerializeField] private Button demoButton;
     [SerializeField] private Button quitButton;
-
+    [SerializeField] private GameObject selectorUIPrefab;
+    private GameObject selectorUI;
+    
     //Data
     private string courseID;
+    private int worldIndex;
+    private Optional<int> dungeonIndex;
     private string startParameters;
     private bool demoMode;
     private AreaInformation currentArea;
@@ -35,11 +39,28 @@ public class AreaGeneratorManager : MonoBehaviour
 
     private void Awake()
     {
-        Setup();
+        if (Instance == null)
+        {
+            Instance = this;
+            Setup();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    private async void Setup()
+    private void Setup()
     {
+        selectorUI = Instantiate(selectorUIPrefab);
+        panel.SetActive(false);
+    }
+
+    private async void SetupGenerator()
+    {
+        Destroy(selectorUI);
+        panel.SetActive(true);
+
 #if UNITY_EDITOR
         courseID = "";
 #else
@@ -123,12 +144,12 @@ public class AreaGeneratorManager : MonoBehaviour
     {
 #if UNITY_EDITOR
         //skipping area retrieval in editor, using area 1-1 instead
-        return new Optional<AreaInformation>(new AreaInformation(1, new Optional<int>(1)));
+        return new Optional<AreaInformation>(new AreaInformation(worldIndex, dungeonIndex));
 #endif
 
         //get gamemode parameter of url
         //string urlPart = Application.absoluteURL.Split("&")[^1];
-        return new Optional<AreaInformation>(new AreaInformation(1, new Optional<int>()));
+        return new Optional<AreaInformation>(new AreaInformation(worldIndex, dungeonIndex));
         /*
         //split in parts
         string[] areaParts = urlPart.Split("-");
@@ -203,6 +224,8 @@ public class AreaGeneratorManager : MonoBehaviour
     private AreaData LoadLocalData()
     {
         string path;
+        Debug.Log("WorldIndex: " + currentArea.GetWorldIndex());
+        Debug.Log("DungeonIndex: " + currentArea.GetDungeonIndex());
         if (currentArea.IsDungeon())
         {
             path = "Areas/Dungeon" + currentArea.GetWorldIndex() + "-" + currentArea.GetDungeonIndex();            
@@ -306,5 +329,19 @@ public class AreaGeneratorManager : MonoBehaviour
         {
             areaManager.SetupGenerator(courseID, this, areaData, currentArea, cameraController, false, demoMode);
         }
+    }
+
+    /// <summary>
+    /// Saves all necessary values for the World Generation and starts it.
+    /// </summary>
+    /// <param name="courseID">ID of the selected course</param>
+    /// <param name="worldIndex">Index of the selected world which should be created</param>
+    /// <param name="dungeonIndex">Index of the dungeon if it should be created</param>
+    public void StartGenerator(string courseID, int worldIndex, Optional<int> dungeonIndex)
+    {
+        this.courseID = courseID;
+        this.worldIndex = worldIndex;
+        this.dungeonIndex = dungeonIndex;
+        SetupGenerator();
     }
 }
--- Assets/Scripts/WorldGeneration/UI/SelectorUI.cs
+++ Assets/Scripts/WorldGeneration/UI/SelectorUI.cs
@@ -11,15 +11,58 @@ using Cysharp.Threading.Tasks;
 
 public class SelectorUI : MonoBehaviour
 {
+
     /// <summary>
     ///     Import of the overworld close methode
     /// </summary>
     [DllImport("__Internal")]
     private static extern void CloseOverworld();
 
+    [SerializeField] private TMP_Dropdown courseIDDropDownMenu;
+    [SerializeField] private TMP_InputField wordlIndexInputField;
+    [SerializeField] private TMP_InputField dungeonIndexInputField;
+
+    private string courseID;
+    private int worldIndex;
+    private Optional<int> dungeonIndex;
+
+    /// <summary>
+    ///     This function resets the content panel
+    /// </summary>
+    public void ResetContentPanel()
+    {
+        courseIDDropDownMenu.value = 0;
+        wordlIndexInputField.text = "";
+        dungeonIndexInputField.text = "";
+    }
+
+    public void OnContinueButtonPressed()
+    {
+        ContinueButton();
+    }
+
+    private void CourseIDDropDownMenu()
+    {
+        string path = GameSettings.GetOverworldBackendPath() + "/courses/";
+        //Optional<> areaDTO = await RestRequest.GetRequest<AreaDTO>(path);
+    }
+
+    /// <summary>
+    /// This function is called by the continue button and saves the entered course ID, dungeon Index (if entered) and world index.
+    /// </summary>
+    /// <returns></returns>
     public async UniTask ContinueButton()
     {
-        await GameSettings.FetchValues();
-        await SceneManager.LoadSceneAsync("AreaGeneratorManager");
+        Debug.Log("Course ID: " + courseIDDropDownMenu.value);
+        Debug.Log("World Index: " + wordlIndexInputField.text);
+        Debug.Log("Dungeon Index: " + dungeonIndexInputField.text);
+
+        // retrieve entered data
+        courseID = courseIDDropDownMenu.value.ToString();
+        worldIndex = int.Parse(wordlIndexInputField.text);
+        dungeonIndex = dungeonIndexInputField.text != "" ? new Optional<int>(int.Parse(dungeonIndexInputField.text)) : new Optional<int>();
+        Debug.Log(AreaGeneratorManager.Instance);
+        AreaGeneratorManager.Instance.StartGenerator(courseID, worldIndex, dungeonIndex);
     }
+
 }
