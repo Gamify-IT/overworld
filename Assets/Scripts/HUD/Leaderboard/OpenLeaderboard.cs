d50759b153e843b7559696c4e36a63e581a8e456
Duyguoe <duyguoezden96@gmail.com>
2024-07-30 17:48:50 +0200

    Implementation of a shop

--- Assets/Scripts/HUD/Leaderboard/OpenLeaderboard.cs
+++ Assets/Scripts/HUD/Leaderboard/OpenLeaderboard.cs
@@ -8,11 +8,42 @@ public class OpenLeaderboard : MonoBehaviour
 {
 
     public static bool menuOpen = true;
+    public GameObject rewardsPanel;
+    public GameObject shopPanel;
 
-    public void Pause()
+    public void openMenue()
+    {
+        menuOpen = true;
+        rewardsPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void openLeaderboard()
     {
         menuOpen = true;
         SceneManager.LoadScene("Rewards", LoadSceneMode.Additive);
         Time.timeScale = 0f;
     }
+
+    public void openShop()
+    {
+        menuOpen = true;
+        shopPanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void closeMenue()
+    {
+        menuOpen = false;
+        rewardsPanel.SetActive(false); 
+        Time.timeScale = 1f; 
+    }
+
+    public void closeShop()
+    {
+        menuOpen = false;
+        shopPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
 }
