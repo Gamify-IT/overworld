914da5287927a6399be9430bd7f41aeaf53f9541
Duyguoe <duyguoezden96@gmail.com>
2024-08-07 21:24:45 +0200

    Updated Frontend Classes

--- Assets/Scripts/HUD/Shop/HandleShopMenu.cs
+++ Assets/Scripts/HUD/Shop/HandleShopMenu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shop : MonoBehaviour
+public class HandleShopMenu : MonoBehaviour
 {
 
     public GameObject ShopPanel;
